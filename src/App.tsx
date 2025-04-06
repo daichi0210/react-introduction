444d72e77069927f6eb92991572e95b95ec91a60
Daichi <kabata0210@icloud.com>
2025-04-06 20:53:04 +0900

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from "react-router-dom"
 
-function App() {
+export const App = () => {
+  const title: string = 'Hello World!';
+
   return (
     <div className='App'>
-      <h1>Hello World!</h1>
+      <h1>{title}</h1>
+      <Link to='/test'>Testへ</Link>
     </div>
-  )
+  );
 }
 
 export default App
