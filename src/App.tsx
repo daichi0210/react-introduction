690e53cecdb673508cfe42319fb57cede44f73ac
Daichi <kabata0210@icloud.com>
2025-04-07 18:31:37 +0900

    React Hooks と state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
+import { useState } from "react"
 import { LinkButton } from "./components/link"
 
 export const App = () => {
   const title: string = 'Hello World!';
+  const [num, setNum] = useState(0);
+
+  const increment = () => {
+    setNum(num + 1);
+  }
 
   return (
     <div className='App'>
       <h1>{title}</h1>
+      {num}回押しました。
       <LinkButton text="Testへ" link="/test" />
+      <button type="button" onClick={increment}>増やす</button>
     </div>
   );
 }
